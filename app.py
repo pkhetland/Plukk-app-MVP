7e61efeb40d4156aba6e9c0e6b5ed05524134a36
Petter Hetland <Pehe@nmbu.no>
2019-10-08 20:58:45 +0200

    Addition changes to visual layout.

--- app.py
+++ app.py
@@ -10,39 +10,29 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 
-navbar = dbc.NavbarSimple(
-    children=[dbc.NavItem(dbc.NavLink("Last ned appen", href="#"))],
-    brand="PLUKK by Bouvet",
-    brand_href="#",
-    sticky="top",
-    color="dark",
-    dark=True,
-    style={"font-family": "helvetica"}
-)
-
 body = dbc.Container(  # Main container
     fluid=True,
     children=[
         dbc.Row(
             [  # Banner first row
                 dbc.Col(  # Banner first column
-                    className="ml-5",
-                    lg=5,
-                    md=8,
+                    lg=4,
+                    md=6,
                     sm=8,
                     children=[
                         html.H1(
-                            "PLUKK.",
+                            "Plukk.",
                             className="mb-5",
                             style={"color": "white",
-                                   "font-size": "4rem"},
+                                   "font-size": "4rem",
+                                   "text-shadow": "0px 3px black"},
                         ),
                         html.P(
                             """\
-PLUKK gjør det morsomt å plukke søppel. Med din egel QR-kode, mobilen din 
-og dine egne poser kan du konkurrere mot vennene dine, 
+Plukk gjør det morsomt å plukke søppel.\n Med din egen QR-kode, en mobil \
+og egne poser kan store og små konkurrere mot venner, samarbeide, \
 vinne dritkule premier og få belønninger.""",
-                            style={"color": "white"},
+                            style={"color": "#fafafa"},
                         ),
                         dbc.Row(
                             [  # Row for buttons within column 1
@@ -51,42 +41,33 @@ vinne dritkule premier og få belønninger.""",
                                         dbc.Button(
                                             "Last ned appen",
                                             # Download button
-                                            color="info",
+                                            color="warning",
                                             size="lg",
-                                            outline=False
+                                            style={"box-shadow": "0px 2px black"}
                                         )
                                     ],
-                                    width=6,
-                                ),
-                                dbc.Col(
-                                    [
-                                        dbc.Button(
-                                            "Lær mer om PLUKK",
-                                            # Read more button
-                                            color="light",
-                                            size="lg",
-                                            outline=True
-                                        )
-                                    ],
-                                    width=6,
-                                ),
+                                    width=12,
+                                )
                             ],
                             className="mt-5",
-                            justify="center",
+                            justify="left",
                             align="center",
                         ),
                     ],
                 ),
                 dbc.Col(  # Banner second column
                     [html.Img(src="/assets/images/three_phones_2.png",
-                              width="100%")],
-                    width="5",
+                              width="100%")
+                     ],
+                    lg="5",
+                    md="6",
+                    sm="12"
                 ),
             ],  # Banner row children closure
             style={
                 "background-image": "url('/assets/images/lofoten_beach_background.png')",
-                "min-height": "650px",
-                "font-family": "courier"},
+                "min-height": "650px"
+            },
             justify="around",
             align="center",
         ),  # Banner row closing parantheses
@@ -96,13 +77,10 @@ vinne dritkule premier og få belønninger.""",
                     [
                         dbc.Col(
                             [  # Main col: Heading
-                                html.H1(
-                                    "Slik fungerer PLUKK.",
-                                    style={"text-align": "center"},
-                                )
+                                html.H1("Slik fungerer PLUKK.", style={"text-align": "center"})
                             ],
                             width=12,
-                            className="mb-5"
+                            className="mb-5",
                         ),
                         dbc.Row(
                             [  # Main col: Row for cards
@@ -111,49 +89,47 @@ vinne dritkule premier og få belønninger.""",
                                         html.Img(
                                             src="/assets/images/bottles_quadratic.png",
                                             width="100%",
-                                            className="mb-5"
+                                            className="mb-5",
                                         ),
-                                        html.H4(
-                                            "1. Plukk søppel i Plukkposer."
-                                        )
+                                        html.H4("1. Plukk søppel i Plukkposer.")
                                     ],
-                                    lg=3
+                                    lg=3,
                                 ),  # First card closing parantheses
                                 dbc.Col(
                                     [  # Second card
                                         html.Img(
                                             src="/assets/images/bin_bag.png",
                                             width="100%",
-                                            className="mb-5"
+                                            className="mb-5",
                                         ),
                                         html.H4(
                                             "2. Scann din personlige QR-kode og kast søpla på best mulig sted."
-                                        )
+                                        ),
                                     ],
-                                    lg=3
+                                    lg=3,
                                 ),  # Second card closing parantheses
                                 dbc.Col(  # Third card
                                     [
                                         html.Img(
                                             src="/assets/images/trophy.png",
                                             width="100%",
-                                            className="mb-5"
+                                            className="mb-5",
                                         ),
                                         html.H4(
                                             "3. Få en poengsum på din konto og bruk dem på markedsplassen vår!"
                                         ),
                                     ],
-                                    lg=3
+                                    lg=3,
                                 ),  # Third card col closing parantheses
                             ],
                             justify="around",
                             align="start",
-                            style={"text-align": "left"}
-                        )  # Card row closing parantheses
-                    ])
+                            style={"text-align": "left"},
+                        ),  # Card row closing parantheses
+                    ]
+                )
             ],  # Properties of about-row
-            style={"min-height": "700px",
-                   "font-family": "courier"},
+            style={"min-height": "700px"},
             align="center",
             justify="center",
         ),  # About row closing parantheses
@@ -166,7 +142,8 @@ vinne dritkule premier og få belønninger.""",
                                 html.H1(
                                     "Hva kan du glede deg til?",
                                     style={"text-align": "center",
-                                           "color": "white"},
+                                           "color": "white",
+                                           "text-shadow": "0px 2px black"},
                                 )
                             ],
                             width=12,
@@ -176,12 +153,13 @@ vinne dritkule premier og få belønninger.""",
                             [  # Main col: Paragraph
                                 html.P(
                                     """\
-PLUKK gjør det morsomt å plukke søppel. Med din egel QR-kode, mobilen din 
-og dine egne poser kan du konkurrere mot vennene dine, 
-vinne dritkule premier og få belønninger.""",
+PLUKK kommer med mange kule funksjoner som eget poengsystem, felles events, \
+personlige utfordringer, lederlister og et skattekart som viser hvor plastavfallet befinner\
+ seg i dag.
+\nHer er noen eksempler:""",
                                     style={"text-align": "center",
-                                           "color": "white"},
-                                    className="mt-5 mb-5"
+                                           "color": "silver"},
+                                    className="mt-2 mb-4",
                                 )
                             ],
                             lg={"size": 6, "offset": 3},
@@ -194,79 +172,148 @@ vinne dritkule premier og få belønninger.""",
                                     [  # First card
                                         html.Img(
                                             src="/assets/images/feature_achievements_glare.png",
-                                            width="100%"
+                                            width="100%",
                                         ),
                                         html.H4(
                                             "Personlige utfordringer og belønninger",
                                             className="mt-3 mb-2",
-                                            style={"color": "white"}
-                                        )
+                                            style={"color": "white",
+                                                   "text-shadow": "0px 1px black"},
+                                        ),
                                     ],
-                                    lg=3
+                                    lg=3,
                                 ),  # First card closing parantheses
                                 dbc.Col(
                                     [  # Second card
                                         html.Img(
-                                            src="/assets/images/feature_map.png",
-                                            width="100%"
+                                            src="/assets/images/feature_map.png", width="100%"
                                         ),
                                         html.H4(
                                             "Et skattekart for plast og Plukk-events",
                                             className="mt-3 mb-2",
-                                            style={"color": "white"}
-                                        )
+                                            style={"color": "white",
+                                                   "text-shadow": "0px 1px black"},
+                                        ),
                                     ],
-                                    lg=3
+                                    lg=3,
                                 ),  # Second card closing parantheses
                                 dbc.Col(  # Third card
                                     [
                                         html.Img(
                                             src="/assets/images/feature_achievements_glare.png",
-                                            width="100%"
+                                            width="100%",
                                         ),
                                         html.H4(
                                             "Et råkult poengsystem med premier",
                                             className="mt-3 mb-2",
-                                            style={"color": "white"}
-                                        )
+                                            style={"color": "white",
+                                                   "text-shadow": "0px 1px black"},
+                                        ),
                                     ],
-                                    lg=3
+                                    lg=3,
                                 ),  # Third card col closing parantheses
                             ],
                             justify="around",
                             align="start",
-                            style={"text-align": "center"}
-                        )  # Card row closing parantheses
-                    ])
+                            style={"text-align": "center"},
+                        ),  # Card row closing parantheses
+                    ]
+                )
             ],  # Properties of feature row
-            style={"min-height": "1200px",
-                   "background-image": "url('assets/images/green_background.png')",
-                   "font-family": "courier"},
-            align="center",
+            style={
+                "min-height": "1000px",
+                "background-image": "url('assets/images/green_background.png')"
+            },
+            align="start",
             justify="center",
         ),  # Feature row closing parantheses
-        dbc.Row([  # Footer row
-            dbc.Col([  # Footer first col
-                html.P("Historien")
-            ],  lg=4,
-                align="center",
-                style={"text-align": "center"}
-            ),
-            dbc.Col([  # Footer second col
-                html.P("Footer text")
+        dbc.Row([  # Signup row
+            dbc.Col([  # First col for signup form
+                html.H1("Bli først til å teste Plukk!",
+                        className="mb-4",
+                        style={"text-shadow": "0px 2px black"}),
+                html.P("""Fyll ut skjemaet under for å få informasjon om\
+                 utviklingen av Plukk eller være én av\
+                 de første til å teste appen.""",
+                       className="mb-4",
+                       style={"color": "teal"}),
+                dbc.Form([  # Signup form
+                    dbc.FormGroup([  # First name input
+                        dbc.Label("Navn",
+                                  html_for="name_row",
+                                  style={"color": "#fafafa"}),
+                        dbc.Input(type="text",
+                                  id="name",
+                                  placeholder="... Skriv inn navnet ditt her",
+                                  style={"width": "80%",
+                                         "box-shadow": "0px 2px black"})
+                    ]),
+                    dbc.FormGroup([
+                        dbc.Label("E-postadresse",
+                                  html_for="email_row",
+                                  style={"color": "#fafafa"}),
+                        dbc.Input(type="email", id="email",
+                                  placeholder="... Skriv inn din e-postadresse her",
+                                  style={"width": "80%",
+                                         "box-shadow": "0px 2px black"})
+                    ]),
+                    dbc.Button("Send inn",
+                               color="warning",
+                               className="mt-4 mb-2",
+                               style={"box-shadow": "0px 2px black"})
+                ])
+            ],  # Signup col properties
+                lg=6,
+                style={"color": "white"}
+            )
+        ],  # Signup row properties
+            align="center",
+            justify="center",
+            style={"min-height": "600px",
+                   "background-color": "#3b3b3b"}
+        ),
+        dbc.Row(
+            [  # Story row
+                dbc.Col([
+                    html.H1("Historien bak Plukk",
+                            className="mb-3",
+                            style={"text-shadow": "0px 2px black"}),
+                    html.P(
+"""En kald februarnatt i 2019 satte et team fra konsulentselskapet Bouvet seg \
+ned for være med å løse et enormt problem: Plast i havet.\n
+I tillegg til et enormt problem hadde de 24 timer på å komme frem til en løsning. Med hjelp \
+fra Plastkoordinator i Oslo, Anja Stokkan, ble Plukk løsningen til teamet. \
+Appen hadde som hensikt å motivere folket til å plukke plast og søppel i \
+naturen ved å skape et pantesystem som belønnet dem for strevet.\n
+Dette er Plukk.
+                           """)
+                     ],  # Story first col
+                    lg=5,
+                    align="center",
+                    style={"text-align": "left"},
+                ),
+                dbc.Col(  # Story second col
+                    [html.Img(src="/assets/images/bouvet_hackathon.jpg",
+                              width="100%",
+                              style={"border-radius": "20px",
+                                     "border": "solid",
+                                     "border-color": "#ffcc00"})],
+                    lg=4,
+                    style={"text-align": "center"}
+                )
+                # Closing parantheses of Story row
             ],
-                lg=4,
-                align="center",
-                style={"text-align": "center"})
-            # Closing parantheses of Footer row
-        ],
-            style={"min-height": "300px",
-                   "background-image": "url('assets/images/green_background.png')"},
+            style={
+                "min-height": "500px",
+                "background-image": "url('/assets/images/green_background.png')",
+                "color": "#fafafa"
+            },
             align="center",
             justify="center",
             # Footer row properties
-        )
+        ),
     ],  # Container children closure
+    style={"font-family": "courier"}
 )  # Container end bracket
 
 app.layout = html.Div([body])
