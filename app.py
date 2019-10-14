838445cbcdce4c914d01111b57fc759c63ce284f
Petter Hetland <Pehe@nmbu.no>
2019-10-14 13:12:21 +0200

    Added Moesif Analytics to app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
+from moesifwsgi import MoesifMiddleware
 # from flask import Flask, request, jsonify
 # from flask_sqlalchemy import SQLAlchemy
-
 
 app = dash.Dash(__name__,
                 suppress_callback_exceptions=True,
@@ -9,3 +9,11 @@ app = dash.Dash(__name__,
 )
 
 server = app.server
+
+moesif_settings = {
+    'APPLICATION_ID': 'eyJhcHAiOiI2MTc6MjIxIiwidmVyIjoiMi4wIiwib3JnIjoiMjA3OjIzNCIsImlhdCI6MTU3MTAxMTIwMH0.vL8LqwLnVjAhfpV9JIgn1ZkAPSatTsEVIk9v8mfzq3M',
+    'CAPTURE_OUTGOING_REQUESTS': False, # Set to True to also capture outgoing calls to 3rd parties.
+    'LOG_BODY': True,
+}
+
+server.wsgi_app = MoesifMiddleware(server.wsgi_app, moesif_settings)
