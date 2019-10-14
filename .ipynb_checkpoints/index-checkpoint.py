90706d2fcd93a13df09a3a33733c6693021ad522
Petter Hetland <Pehe@nmbu.no>
2019-10-14 10:30:55 +0200

    Changed contact table to plukk_contacts.

---
+++ .ipynb_checkpoints/index-checkpoint.py
@@ -0,0 +1,13 @@
+import dash_html_components as html
+
+from app import app
+from apps import layout
+from apps import callbacks
+
+body = layout.body  # Get layout from layout.py
+
+app.layout = html.Div([body])  # Define layout
+
+
+if __name__ == "__main__":  # Run program
+    app.run_server(debug=True)
