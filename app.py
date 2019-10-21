127856d61ed1633b8e6ab9974cc4347856ab3fe6
Petter Hetland <Pehe@nmbu.no>
2019-10-21 20:50:32 +0200

    Added Google Analytics tracking code to <head>

--- app.py
+++ app.py
@@ -39,6 +39,15 @@ app.index_string = '''
         &noscript=1"/>
         </noscript>
         <!-- End Facebook Pixel Code -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150603771-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'UA-150603771-1');
+        </script>
     </head>
     <body>
         <header class="container">
